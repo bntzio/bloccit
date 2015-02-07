3623dff0a9287a307e31751a9f7c6453d9850cda
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-02-06 22:23:46 -0600

    Created Post controller and modified routes appropriately

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
+
+  resources :posts
+
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 
