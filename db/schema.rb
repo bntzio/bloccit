c1bbc32b107f3657473e5d33dd7a5756ff8794e7
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-02-11 14:44:17 -0600

    Associating users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209235045) do
+ActiveRecord::Schema.define(version: 20150211201041) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20150209235045) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
