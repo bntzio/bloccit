dc5595e4a99e1d9286295e61da0e1bda5c8e6ec2
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-02-12 20:56:01 -0600

    Fix a migration bug and installs better errors gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ end
 group :development do
   gem 'sqlite3'
   gem 'rails-erd'
+  gem 'better_errors'
 end
 
 gem 'bootstrap-sass'
--- db/migrate/20150212022756_add_role_to_users.rb
+++ db/migrate/20150212022756_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :role, :String
+    add_column :users, :role, :string
   end
 end
