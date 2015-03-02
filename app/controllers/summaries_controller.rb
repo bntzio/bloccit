1efbb38209bf5a590d43cb1c645a7ba32de1e4ec
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-02 12:49:57 -0600

    Progress two on creating summaries for posts

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -6,24 +6,28 @@ class SummariesController < ApplicationController
 
   def new
     @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
     @summary = Summary.new
     authorize @summary
   end
 
   def show
     @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
     @summary = Summary.find(params[:id])
   end
 
   def edit
     @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
     @summary = Summary.find(params[:id])
     authorize @summary
   end
 
   def create
     @post = Post.find(params[:post_id])
-    @summary = current_user.summaries.build(params.require(:post).permit(:title, :body))
+    @topic = Topic.find(params[:topic_id])
+    @summary = current_user.posts.build(params.require(:summary).permit(:description))
     @summary.post = @post
     authorize @summary
 
@@ -38,6 +42,7 @@ class SummariesController < ApplicationController
 
   def update
     @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
     @summary = Summary.find(params[:id])
     authorize @summary
 
