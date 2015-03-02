2b1d6c15477dd92a4cbf8b9474ea76a1a4d7bd1a
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-02 17:47:55 -0600

    Complete Topics and Posts Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   resources :topics do 
     resources :posts, except: [:index] do
-      resource :summary
+      resources :summaries, except: [:index]
     end
   end
   
