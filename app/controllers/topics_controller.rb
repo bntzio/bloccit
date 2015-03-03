1e48da92a13922c86aa4969caa929565097483dc
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-02 22:13:12 -0600

    Extract Method Refactoring on TopicsController

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,7 +21,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    @topic = Topic.new(post_params)
     authorize @topic
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -34,12 +34,16 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+    if @topic.update_attributes(post_params)
       redirect_to @topic
     else
       flash[:error] = "Error saving topic. Please try again."
       render :edit
     end
+  end
+
+  def post_params
+    params.require(:topic).permit(:name, :description, :public)
   end
 
 end
