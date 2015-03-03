db63a1e1b9c4af2538527f27611fd137fc2ac727
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-03 12:37:04 -0600

    Validates posts and uses partials

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,10 @@ class Post < ActiveRecord::Base
   has_one :summary
 
   default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
 end
