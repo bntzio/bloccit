6193c88020fc3d81d0313a2a1e3573a0f6c4007f
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-17 15:39:54 -0600

    Complete Mocking-with-RSpec assignment

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,17 +4,27 @@
  
    include TestFactories
    include Devise::TestHelpers
+
+   before do
+    request.env["HTTP_REFERER"] = '/'
+    @user = authenticated_user
+    @post = associated_post
+    sign_in @user
+   end
  
    describe '#up_vote' do
      it "adds an up-vote to the post" do
-       request.env["HTTP_REFERER"] = '/'
-       @user = authenticated_user
-       @post = associated_post
-       sign_in @user
- 
        expect {
          post( :up_vote, post_id: @post.id )
        }.to change{ @post.up_votes }.by 1
      end
    end
+
+   describe '#down_vote' do
+    it "down-votes a post" do
+      expect {
+         post( :down_vote, post_id: @post.id )
+       }.to change{ @post.down_votes }.by 1
+    end
+  end
  end
