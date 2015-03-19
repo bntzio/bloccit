33e685587df8ab511cb76f8198122ed917c699ba
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-18 18:34:31 -0600

    Complete another interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,13 @@ module ApplicationHelper
        content_tag :div, capture(&block), class: 'form-group'
      end
    end
+
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+
+  def down_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
    
 end
