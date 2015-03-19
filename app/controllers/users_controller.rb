644ddefae5dec3456cf4dd479818b9f621a5920c
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-18 20:08:28 -0600

    Complete public profile checkpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.visible_to(current_user)
+    @comments = @user.comments
+  end
 
   def update
     if current_user.update_attributes(user_params)
--- app/models/post.rb
+++ app/models/post.rb
@@ -32,6 +32,7 @@ class Post < ActiveRecord::Base
   end
 
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
