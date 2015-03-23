665c7d9f4991bbf2bea21a2edb064bd216b1c002
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-03-23 11:59:49 -0600

    Complete popular posts checkpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.visible_to(current_user)
